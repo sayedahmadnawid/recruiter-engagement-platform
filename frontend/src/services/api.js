7820234eb0bb2f3eda1e6faa88342343ba84c793
Sayed <sayed_sayedzada@yahoo.com>
2026-06-13 11:53:56 -0400

    Frontend application configuration for dev and prodution environment

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,8 +1,7 @@
 import axios from "axios";
-//import { redirect } from 'react-router-dom';
 
 const api = axios.create({
-  baseURL: "http://localhost:8000/api",
+  baseURL: import.meta.env.VITE_API_URL,
   headers: {
     "Content-Type": "application/json",
   },
